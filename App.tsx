4f0f670c1f2decffc3c81080affd1835427b0919
Fritzcho <alireza.ramezani@weknowit.se>
2022-04-11 08:50:32 +0200

    Removed some of the files from the expo template.
    Created a src directory for new components, screens and constants that will be created.
    
    Created and styled a TouchableHighlight component.

--- App.tsx
+++ App.tsx
@@ -1,22 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import HighlightButton from './src/constants/StandardButton';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Hello header!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>
+          Lorum ipsum
+        </Text>
+      </View>
+      <HighlightButton content='Test button'/>
+      <Text>Hello World!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#E7E7E7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'green',
+    padding: 40,
+  },
+  body: {
+    backgroundColor: 'white',
+  }
+});
